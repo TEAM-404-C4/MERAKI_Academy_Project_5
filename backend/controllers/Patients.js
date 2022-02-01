0c38cc463fa15d0065e3713e0578d15018f17b10
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-01 22:16:36 +0200

    controllers

---
+++ backend/controllers/Patients.js
@@ -0,0 +1,8 @@
+const connection = require("../db/db");
+
+//This function creates a new Patient (new user)
+const createNewPatient = () => {};
+
+module.exports = {
+  createNewPatient,
+};
---
+++ backend/controllers/Roles.js
@@ -0,0 +1,8 @@
+const connection = require("../db/db");
+
+// This function creates new role
+const createNewRole = () => {};
+
+module.exports = {
+  createNewRole,
+};
