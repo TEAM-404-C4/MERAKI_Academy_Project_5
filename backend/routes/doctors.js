ef3c1b1591c0ed8577e910a1ccf51ed38fe2f88d
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-02 01:03:51 +0200

    Modified Server.js

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+// Import doctors controllers
+const { createNewDoctor } = require("../controllers/Doctors");
+
+// Create doctor router
 const doctorRouter = express.Router();
 
 doctorRouter.get("/", (req, res) => {
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-const registerRouter = express.Router();
+
+
 
 registerRouter.get("/", (req, res) => {
   res.json("registerRouter");
