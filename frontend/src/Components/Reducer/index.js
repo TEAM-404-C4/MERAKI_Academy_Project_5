cd75f24abeaaafc32fd11ff4e424f9c7dd917026
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-02-03 00:59:53 +0200

    index reducer modify

--- frontend/src/Components/Reducer/index.js
+++ frontend/src/Components/Reducer/index.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
+import loginReducer from "./login/index"
 
-const reducers = combineReducers({});
+const reducers = combineReducers({ loginReducer });
 
 const store = createStore(
   reducers,
