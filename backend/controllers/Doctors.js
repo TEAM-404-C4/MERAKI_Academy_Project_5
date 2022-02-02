cd25cb0b7a1809a6a06d59fe37986358f8709c96
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-02-02 02:43:54 +0200

    CreateNewDoctors

--- backend/controllers/Doctors.js
+++ backend/controllers/Doctors.js
@@ -1,7 +1,32 @@
 const connection = require("../database/db");
 
 //Create New Doctors
-const createNewDoctor = () => {};
+const createNewDoctor = (req, res) => {
+  const query = "insert into doctor (firstName,lastName,email,password,profileImage,gender,Nationality,specialization,phone,workingDays,address,careersLicense,waitingTime,consultationFee,departmentId,cityId,roleId,ScientificCertificate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+  const { firstName, lastName, email, password, profileImage, gender, Nationality, specialization, phone, workingDays, address, careersLicense, waitingTime, consultationFee, departmentId, cityId, roleId, ScientificCertificate } = req.body;
+  const data = [
+    firstName, lastName, email, password,
+     profileImage, gender, Nationality,
+      specialization, phone, workingDays,
+       address, careersLicense, waitingTime, consultationFee, departmentId, 
+    cityId, roleId, ScientificCertificate
+  ];
+  connection.query(query, data,(err, result) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+    // result are the data returned by mysql server
+    res.status(201).json({
+      success: true,
+      massage: "All the Doctors",
+      results: result,
+    });
+  });
+};
 
 module.exports = {
   createNewDoctor,
