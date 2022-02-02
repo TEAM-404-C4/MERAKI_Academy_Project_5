03802fb9b44aa82c9128a44b55c6ce5b47c94cb1
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-02 02:49:20 +0200

    create role schema

--- backend/controllers/Roles.js
+++ backend/controllers/Roles.js
@@ -2,10 +2,23 @@ const connection = require("../database/db");
 
 // This function creates new role
 const createNewRole = (req, res) => {
-  const query = ``;
-  const data = [];
+  const role = req.body.role;
+  const query = `INSERT INTO role (name) VALUE (?)`;
+  const data = [role];
   connection.query(query, data, (err, result) => {
-    res.josn(result);
+    console.log(result);
+    if (!err) {
+      res.status(201).json({
+        success: true,
+        message: "Success role created",
+        result: result,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "SERVER ERROR",
+      });
+    }
   });
 };
 
