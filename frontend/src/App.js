46d45c2832ca9ba876ba920e6640b13652286a00
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-02-03 02:08:12 +0200

    rigister patient

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <h1>Start project 5</h1>
+      <Register />
     </div>
   );
 }
--- frontend/src/Components/Reducer/login/index.js
+++ frontend/src/Components/Reducer/login/index.js
@@ -12,7 +12,7 @@ const initialState = {
 const loginReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case "LOG_IN":
-      localStorage.setItem("token", token);
+      localStorage.setItem("token", payload.token);
       return {
         token: payload.token,
         isLoggedIn: true,
@@ -29,8 +29,7 @@ const loginReducer = (state = initialState, { type, payload }) => {
         roleId: "",
       };
     default:
-      return;
-      state;
+      return state;
   }
 };
 
