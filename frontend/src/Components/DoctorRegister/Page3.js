8596e2ef70d8e13aa7b3140d82961f238a0dd5e4
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-05 05:40:44 +0200

    Upload on doctor register page 3 css file

--- frontend/src/Components/DoctorRegister/Page3.js
+++ frontend/src/Components/DoctorRegister/Page3.js
@@ -1,53 +1,90 @@
-import React,{useState} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch,useSelector } from 'react-redux';
-import {addInfoPage} from "../Reducer/DoctorRegister/index"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addInfoPage } from "../Reducer/DoctorRegister/index";
+import "./Page3.css";
+import {
+  BsFillArrowRightCircleFill,
+  BsFillArrowLeftCircleFill,
+} from "react-icons/bs";
 
 const Page3 = () => {
+  const [workingDays, setWorkingDays] = useState("");
+  const [address, setAddress] = useState("");
+  const [careersLicense, setCareersLicense] = useState("");
+  const [waitingTime, setWaitingTime] = useState("");
 
+  // =========================================
 
-    const [workingDays, setWorkingDays] = useState("");
-    const [address, setAddress] = useState("");
-    const [careersLicense, setCareersLicense] = useState("");
-    const [waitingTime, setWaitingTime] = useState("");
+  const dispatch = useDispatch();
+  const history = useNavigate();
 
-    // =========================================
-    
-    const dispatch = useDispatch()
-    const history=useNavigate()
+  const nextButton = async () => {
+    await dispatch(
+      addInfoPage({ workingDays, address, careersLicense, waitingTime })
+    );
+    history("/doctorsignup4");
+  };
 
-    const nextButton = async()=>{
-
-       await dispatch(addInfoPage({workingDays,address,careersLicense,waitingTime}))
-       history("/doctorsignup4")
-
-    }
-
-  return <div>
-
-<input placeholder='WORKING DAYS' type="text" className='workingDaysDoctorRegister' onChange={(e)=>{
-    setWorkingDays(e.target.value)
-}}/>
-<input placeholder='ADDRESS' type="text" className='addressDoctorRegister' onChange={(e)=>{
-    setAddress(e.target.value)
-}}/>
-<input placeholder='CAREERS LICENSE' type="text" className='careersLicenseDoctorRegister' onChange={(e)=>{
-
-setCareersLicense(e.target.value)
-}} />
-<input placeholder='WATING TIME' type="text" className='waitingTimeDoctorRegister' onChange={(e)=>{
-    setWaitingTime(e.target.value)
-}}/> <br />
-
-<button className='backButton' onClick={()=>{
-
-history("/doctorsignup2")
-
-}}>BACK</button>
-<button onClick={nextButton}>NEXT</button>
-
-
-  </div>;
+  return (
+    <>
+      <div className="mainPage3">
+        <div className="Page3">
+          <label className="levelLabel3">
+            {" "}
+            Working Days , Address , Licenes and Wating - Step 3 of 4
+          </label>
+          <input
+            placeholder="Working Days"
+            type="text"
+            className="doctorWorkingDays"
+            onChange={(e) => {
+              setWorkingDays(e.target.value);
+            }}
+          />
+          <input
+            placeholder="Address"
+            type="text"
+            className="doctorAddress"
+            onChange={(e) => {
+              setAddress(e.target.value);
+            }}
+          />
+          <input
+            placeholder="Careers Licenes"
+            type="text"
+            className="doctorCareersLicense"
+            onChange={(e) => {
+              setCareersLicense(e.target.value);
+            }}
+          />
+          <input
+            placeholder="Wating Time"
+            type="text"
+            className="doctorWaitingTime"
+            onChange={(e) => {
+              setWaitingTime(e.target.value);
+            }}
+          />{" "}
+          <br />
+          <div className="nextAndBackBtn">
+            <button
+              onClick={nextButton}
+              className="backBtn"
+              onClick={() => {
+                history("/doctorsignup2");
+              }}
+            >
+              <BsFillArrowLeftCircleFill />
+            </button>
+            <button onClick={nextButton} className="nextBtn">
+              <BsFillArrowRightCircleFill />
+            </button>
+          </div>
+        </div>{" "}
+      </div>{" "}
+    </>
+  );
 };
 
 export default Page3;
