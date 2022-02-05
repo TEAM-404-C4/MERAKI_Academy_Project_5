42179d067db066f9a85b2d7ba1817f582cfb0a2a
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-05 23:51:46 +0200

    clean Code Card Doctor ,filte ,mainPage and search

--- frontend/src/Components/MainPage/MainPage.js
+++ frontend/src/Components/MainPage/MainPage.js
@@ -1,32 +1,33 @@
+//====================================================//Require
 import React, { useState, useEffect } from "react";
 import CardDoctor from "./CardDoctor";
 import axios from "axios";
 import Search from "./Search";
-import './style.css'
+import "./style.css";
 import Filter from "./Filter";
-export default function MainPage() {
+
+//====================================================//Create Main Page Funtion
+const MainPage = () => {
   const [doctors, setDoctors] = useState([]);
   const [message, setMessage] = useState("");
   const [search, setSearch] = useState("");
   // Status For Pagination
   const [itemsPerPage, setitemsPerPage] = useState(5);
-const [currentPage, setcurrentPage] = useState(1);
+  const [currentPage, setcurrentPage] = useState(1);
 
   const [pageNumberLimit, setpageNumberLimit] = useState(5);
   const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
   const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
-  // =====================================
+  // Use Effect
   useEffect(() => {
     getAllDoctors();
   }, []);
 
-  // =================================
-
+  //====================================================//Create Get All Doctors
   const getAllDoctors = async () => {
     try {
       const res = await axios.get("http://localhost:5000/doctors");
       if (res.data.success) {
-        console.log(res.data.results);
         setDoctors(res.data.results);
         setMessage("");
       } else throw Error;
@@ -37,7 +38,6 @@ const [currentPage, setcurrentPage] = useState(1);
       setMessage("Error happened while Get Data, please try again");
     }
   };
-  // =======================
   const pages = [];
   for (let i = 1; i <= Math.ceil(doctors.length / itemsPerPage); i++) {
     pages.push(i);
@@ -45,11 +45,11 @@ const [currentPage, setcurrentPage] = useState(1);
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = doctors.slice(indexOfFirstItem, indexOfLastItem);
-// list bottom numbers pagination 
-const handleClick = (event) => {
+  // list bottom numbers pagination
+  const handleClick = (event) => {
     setcurrentPage(Number(event.target.id));
   };
-const renderPageNumbers = pages.map((number) => {
+  const renderPageNumbers = pages.map((number) => {
     if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
       return (
         <li
@@ -94,28 +94,27 @@ const renderPageNumbers = pages.map((number) => {
   const handleLoadMore = () => {
     setitemsPerPage(itemsPerPage + 5);
   };
-  const renderData =(data)=>{
+  const renderData = (data) => {
     let doctorCard = data.map((card) => {
-    return (
-      <>
-        <CardDoctor
-          key={card.id}
-          id={card.id}
-          fullName={card.fullName}
-          address={card.address}
-          profileImage={card.profileImage}
-          consultationFee={card.consultationFee}
-          department={card.departmentId}
-          ScientificCertificate={card.scientificCertificate}
-        />
-
-      </>
-    );
-  });
-  return doctorCard;
+      //======================================================//Return
+      return (
+        <>
+          <CardDoctor
+            key={card.id}
+            id={card.id}
+            fullName={card.fullName}
+            address={card.address}
+            profileImage={card.profileImage}
+            consultationFee={card.consultationFee}
+            department={card.departmentId}
+            ScientificCertificate={card.scientificCertificate}
+          />
+        </>
+      );
+    });
+    return doctorCard;
   };
-  
-  // ===========================
+
   let seachDoctorCard = search
     ? search.map((card) => {
         return (
@@ -137,9 +136,7 @@ const renderPageNumbers = pages.map((number) => {
 
   return (
     <div>
-      
-
-      <Filter setSearch={setSearch}/>
+      <Filter setSearch={setSearch} />
       <Search setSearch={setSearch} />
 
       {search ? seachDoctorCard : renderData(currentItems)}
@@ -163,10 +160,10 @@ const renderPageNumbers = pages.map((number) => {
           >
             Next
           </button>
-          
         </li>
       </ul>
-      
     </div>
   );
-}
+};
+
+export default MainPage;
