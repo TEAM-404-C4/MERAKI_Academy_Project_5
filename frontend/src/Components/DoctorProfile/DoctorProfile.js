316b8bf424262037f52728982e30a1f5fb0f9e92
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-05 23:01:55 +0200

    Clean Code in page 1,page 2,page 3,page 4 and doctorProfile.js

--- frontend/src/Components/DoctorProfile/DoctorProfile.js
+++ frontend/src/Components/DoctorProfile/DoctorProfile.js
@@ -1,63 +1,46 @@
-import axios from 'axios';
-import React,{useState,useEffect} from 'react';
-import { useDispatch,useSelector } from 'react-redux';
+//====================================================//Require
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
-
+//====================================================//Doctor Profile Function
 const DoctorProfile = () => {
+  //UseState
+  const [doctor, setDoctor] = useState("");
+  //Selector
+  const state = useSelector((state) => {
+    return state.doctorsReducer;
+  });
+  //UseEffect
+  useEffect(async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:5000/doctors/${state.doctorId}`
+      );
+      setDoctor(res.data.result[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-const [doctor, setDoctor] = useState("");
-
-
-
-const state = useSelector((state)=>{
-
-    return state.doctorsReducer
-})
-// ========================================
-
-
-
-useEffect(async() => {
-try{
-
-    const res = await axios.get(`http://localhost:5000/doctors/${state.doctorId}`)
-    
-    setDoctor(res.data.result[0])
-    console.log(res.data.result[0]);
-}
-
-  catch(err){
-
-    console.log(err);
-  }
-}, []);
-
-
-
-// ====================================
-
-
-
-  return <div>
-
-    <img src={doctor.profileImage} />
-    <p>fullName{doctor.fullName} </p>
-    <p>ScientificCertificate{doctor.ScientificCertificate} </p>
-    <p>Nationality{doctor.Nationality} </p>
-    <p>careersLicense{doctor.careersLicense} </p>
-    <p>address{doctor.address} </p>
-    <p>Name{doctor.Name} </p>
-    <p>email{doctor.email} </p>
-    <p>phone{doctor.phone} </p>
-    <p>specialization{doctor.specialization} </p>
-    <p>waitingTime{doctor.waitingTime} </p>
-    <p>workingDays{doctor.workingDays} </p>
-    <p>consultationFee{doctor.consultationFee} </p>
-
-
-
-
-  </div>;
+  //====================================================//Return
+  return (
+    <div>
+      <img src={doctor.profileImage} />
+      <p>fullName{doctor.fullName} </p>
+      <p>ScientificCertificate{doctor.ScientificCertificate} </p>
+      <p>Nationality{doctor.Nationality} </p>
+      <p>careersLicense{doctor.careersLicense} </p>
+      <p>address{doctor.address} </p>
+      <p>Name{doctor.Name} </p>
+      <p>email{doctor.email} </p>
+      <p>phone{doctor.phone} </p>
+      <p>specialization{doctor.specialization} </p>
+      <p>waitingTime{doctor.waitingTime} </p>
+      <p>workingDays{doctor.workingDays} </p>
+      <p>consultationFee{doctor.consultationFee} </p>
+    </div>
+  );
 };
 
 export default DoctorProfile;
