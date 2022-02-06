31699faf58fdc3aec36bdd881866fe8297091a50
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-02-06 23:45:21 +0200

    clean code

--- backend/controllers/Doctors.js
+++ backend/controllers/Doctors.js
@@ -121,7 +121,6 @@ const getDoctorById = (req, res) => {
 const getDoctorByDepartment = (req, res) => {
   const { city, department } = req.body;
   let subQuery;
-  console.log(city, department);
   let data = [department, city];
   if (city != 0 && department != 0) {
     subQuery = `departmentId = ? AND cityId = ?`;
@@ -141,7 +140,6 @@ const getDoctorByDepartment = (req, res) => {
   // const query = `SELECT * FROM healthcare.doctor  ${subQuery} Join healthcare.city on healthcare.city.id=healthcare.doctor.cityId Join healthcare.medicaldepartment on healthcare.medicaldepartment.id=healthcare.doctor.departmentId`;
   const query = `SELECT healthcare.doctor.id,healthcare.doctor.fullName,healthcare.doctor.email,healthcare.doctor.password,healthcare.doctor.profileImage,healthcare.doctor.gender,healthcare.doctor.status,healthcare.doctor.Nationality,healthcare.doctor.specialization,healthcare.doctor.phone,healthcare.doctor.workingDays,healthcare.doctor.address,healthcare.doctor.careersLicense,healthcare.doctor.waitingTime,healthcare.doctor.consultationFee,healthcare.doctor.ScientificCertificate,healthcare.city.Name as 'city',healthcare.medicaldepartment.Name as 'Department' FROM healthcare.doctor Join healthcare.city on healthcare.city.id=healthcare.doctor.cityId Join healthcare.medicaldepartment on healthcare.medicaldepartment.id=healthcare.doctor.departmentId  where doctor.is_deleted = 0 and (${subQuery})`;
   connection.query(query, data, (err, result) => {
-    console.log(result);
     if (err) {
       return res.json(err);
     }
