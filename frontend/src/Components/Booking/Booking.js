16ca40a4adf69c52a4f016b47eabcc3cd466c202
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-02-07 07:53:46 +0200

    edit time convert style from 12 to 12.00

--- frontend/src/Components/Booking/Booking.js
+++ frontend/src/Components/Booking/Booking.js
@@ -4,27 +4,25 @@ export default function Booking() {
     const [message,setMessage]=useState('lala');
    const book =()=>{
        let arr=[];
-       let start=12;
-       let end=17;
+       let start=12.00;
+       let end=17.00;
        let time=end-start;
        for (let index = start; index < end; index++) {
            let element0 = index;
          for (let index = 0; index < 4; index++) {
              if (index==0) {
               
-             const element = element0;
-             element0 = element;
-             arr.push(element.toLocaleString(undefined,{maximumFractionDigits:3}));   
+             arr.push(element0.toLocaleString(undefined,{minimumFractionDigits:2}));   
              }
              else{
                  
              const element = element0+0.15;
              element0 = element;
-             arr.push(element.toLocaleString(undefined,{maximumFractionDigits:3}));
+             arr.push(element.toLocaleString(undefined,{minimumFractionDigits:2}));
              }
          }  
        }
-       arr.push(end.toString());
+       arr.push(end.toString()+':00');
        let date=arr.map((element, index) =>{
            return <option value={element} key={index} style={{color:'red'}}>{element}</option>
        })
