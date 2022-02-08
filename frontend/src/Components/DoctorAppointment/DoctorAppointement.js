8488736cbacd036a68a1f3fb3beca84e4ba29c82
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-02-09 00:32:29 +0200

    create doctorApointement comp modify

--- frontend/src/Components/DoctorAppointment/DoctorAppointement.js
+++ frontend/src/Components/DoctorAppointment/DoctorAppointement.js
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const DoctorAppointement = () => {
-  return <div>heeeeeeeeeeeeello</div>;
+  const [appointementTime, setAppointementTime] = useState([]);
+  const [patientPhone, setPatientPhone] = useState([]);
+  const [patientFirstName, setPatientFirstName] = useState([]);
+  const [patientLastName, setPatientLastName] = useState([]);
+
+  useEffect(() => {
+    getAppointement();
+  }, []);
+
+  const getAppointement = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:5000/doctors/getappointement"
+      );
+      console.log(res.data.result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      {appointementTime.map((element, index) => {
+        return (
+          <div>
+            <p> {element} </p>
+            <p>{patientPhone[index]}</p>
+            <p>{`${patientFirstName[index]}  ${patientLastName[index]} `}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default DoctorAppointement;
