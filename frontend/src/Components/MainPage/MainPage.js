83cad8d1fe37c9cf28df38555ede2dcb50911734
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-09 01:00:28 +0200

    Update on ssearch and filter bar

--- frontend/src/Components/MainPage/MainPage.js
+++ frontend/src/Components/MainPage/MainPage.js
@@ -28,7 +28,6 @@ const MainPage = () => {
   // Use Effect
   useEffect(() => {
     getAllDoctors();
-
   }, []);
 
   //====================================================//Create Get All Doctors
