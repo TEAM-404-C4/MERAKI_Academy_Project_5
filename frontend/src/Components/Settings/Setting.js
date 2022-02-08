c1c0afbd1c946f1fa760cfa486bed06682080f1c
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-02-08 03:26:17 +0200

    get user id from auth & change endpoint

--- frontend/src/Components/Settings/Setting.js
+++ frontend/src/Components/Settings/Setting.js
@@ -2,42 +2,6 @@ import React from "react";
 import "./Setting.css";
 
 const Setting = () => {
-  const changePassword = async (e) => {
-    e.preventDefault();
-    try {
-      const result = await axios.put(
-        "http://localhost:5000/users/ChangePassword",
-        { oldPassword, newPassword },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      if (result.data.success) {
-        const myTimeout = setTimeout(logout, 2000);
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: result.data.message,
-          showConfirmButton: false,
-          timer: 2000,
-        });
-      } else throw Error;
-    } catch (error) {
-      if (error.response && error.response.data) {
-        Swal.fire({
-          position: "center",
-          icon: "warning",
-          title: error.response.data.message,
-          showConfirmButton: false,
-          timer: 2000,
-        });
-      }
-    }
-  };
-
   return (
     <>
       <div className="changePasswordDiv">
