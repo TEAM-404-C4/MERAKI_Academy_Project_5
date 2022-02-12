11b8ea796201e05636d8804a294ae9eae222fa7b
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-02-12 02:54:59 +0200

    edit in full calendar

--- frontend/src/Components/FullCalender/FullCalender.js
+++ frontend/src/Components/FullCalender/FullCalender.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import { useSelector } from "react-redux";
+
 import "@fullcalendar/timegrid/main.css";
 import "@fullcalendar/daygrid/main.css";
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 export default function FullCalender() {
-  const events = [{ title: "Today", date: new Date() }];
+  const [appointement, setAppointement] = useState([]);
 
+    const state = useSelector((state) => {
+        return {
+          doctorId: state.doctorsReducer,
+          userId: state.loginReducer.userId,
+          roleId: state.loginReducer.roleId,
+        };
+      });
+    useEffect(() => {
+        getAppointement();
+      }, []);
+    const getAppointement = async () => {
+        try {
+          const res = await axios.post(
+            "http://localhost:5000/doctors/getappointement",
+            {
+              doctorId: state.userId | window.localStorage.getItem('userId'),
+            }
+          );
+          console.log(res);
+          console.log(res.data.result);
+          setAppointement(res.data.result);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+  const events = [{ title: "Today", date: new Date() }];
+const handleDateClick=(dateClickInfo)=>{
+  console.log(dateClickInfo);
+}
+const retrieveData = async(from,to,callback)=>{
+callback()
+
+}
+const getData=(fetchInfo,callback)=>{
+retrieveData(fetchInfo.start,fetchInfo.end,callback);
+}
   return (
     <div>
       <div className="container">
@@ -17,15 +59,16 @@ export default function FullCalender() {
           </div>
         </div>
         <FullCalendar
-          defaultView="timeGridDay"
+          initialView= 'dayGridMonth'
           on
           header={{
             left: "prev,next",
             center: "title",
             right: "dayGridMonth,timeGridWeek,timeGridDay",
           }}
-          plugins={[dayGridPlugin, timeGridPlugin]}
-          events={events}
+          plugins={[dayGridPlugin, timeGridPlugin,interactionPlugin]}
+          // events={events} onClick={(e) =>{console.log(e.target)}}
+          dateClick={handleDateClick}
         />
       </div>
     </div>
