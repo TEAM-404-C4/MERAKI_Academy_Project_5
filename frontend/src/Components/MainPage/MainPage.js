534dfb1b0b68607da7824012ba740c56fc23fe63
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-02-15 01:10:52 +0200

    Solving Errors in Dashbored , Test Chart ,Remove My Doctor Profile From Navigation bar,Remove Dublicate Style in main page

--- frontend/src/Components/MainPage/MainPage.js
+++ frontend/src/Components/MainPage/MainPage.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import CardDoctor from "./CardDoctor";
 import axios from "axios";
 import Search from "./Search";
-import "./style.css";
 import Filter from "./Filter";
 import { setDoctor } from "../Reducer/Doctor/";
 import { useDispatch, useSelector } from "react-redux";
