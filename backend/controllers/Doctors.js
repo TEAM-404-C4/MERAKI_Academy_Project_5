a421cf563f097ceb1f4b2a2bc843e508afe89092
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-02-17 22:03:19 +0200

    Doctor controller

--- backend/controllers/Doctors.js
+++ backend/controllers/Doctors.js
@@ -75,18 +75,19 @@ const createNewDoctor = async (req, res) => {
 };
 // ==========================================Get All Doctor in Admin Panel
 const getAllDoctorsInAdmin = (req, res) => {
-  const query = "SELECT healthcare.doctor.id,healthcare.doctor.fullName,healthcare.doctor.email,healthcare.doctor.gender,healthcare.doctor.Nationality,healthcare.doctor.phone,healthcare.city.Name as 'city',healthcare.medicaldepartment.Name as 'Department'  FROM doctor Join healthcare.city on healthcare.city.id=healthcare.doctor.cityId Join healthcare.medicaldepartment on healthcare.medicaldepartment.id=healthcare.doctor.departmentId where doctor.is_deleted = 0 ";
+  const query =
+    "SELECT healthcare.doctor.id,healthcare.doctor.fullName,healthcare.doctor.email,healthcare.doctor.gender,healthcare.doctor.Nationality,healthcare.doctor.phone,healthcare.city.Name as 'city',healthcare.medicaldepartment.Name as 'Department'  FROM doctor Join healthcare.city on healthcare.city.id=healthcare.doctor.cityId Join healthcare.medicaldepartment on healthcare.medicaldepartment.id=healthcare.doctor.departmentId where doctor.is_deleted = 0 ";
   connection.query(query, (err, result) => {
     if (err) {
-      res.status(500).json({
+      return res.status(500).json({
         success: false,
         massage: "server error",
         err: err,
       });
     }
-    // console.log(result);
+
     // result are the data returned by mysql server
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       massage: "All the Doctors",
       results: result,
@@ -99,15 +100,15 @@ const getAllDoctors = (req, res) => {
     "SELECT healthcare.doctor.id,healthcare.doctor.fullName,healthcare.doctor.email,healthcare.doctor.password,healthcare.doctor.profileImage,healthcare.doctor.gender,healthcare.doctor.status,healthcare.doctor.Nationality,healthcare.doctor.specialization,healthcare.doctor.phone,healthcare.doctor.workingDays,healthcare.doctor.address,healthcare.doctor.careersLicense,healthcare.doctor.waitingTime,healthcare.doctor.consultationFee,healthcare.doctor.latitude,healthcare.doctor.longitude,healthcare.doctor.ScientificCertificate,healthcare.city.Name as 'city',healthcare.medicaldepartment.Name as 'Department' FROM healthcare.doctor Join healthcare.city on healthcare.city.id=healthcare.doctor.cityId Join healthcare.medicaldepartment on healthcare.medicaldepartment.id=healthcare.doctor.departmentId where healthcare.doctor.is_deleted = 0 ";
   connection.query(query, (err, result) => {
     if (err) {
-      res.status(500).json({
+      return res.status(500).json({
         success: false,
         massage: "server error",
         err: err,
       });
     }
-    // console.log(result);
+
     // result are the data returned by mysql server
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       massage: "All the Doctors",
       results: result,
@@ -157,8 +158,7 @@ const getDoctorByDepartment = (req, res) => {
       "  departmentId =  ALL (SELECT departmentId FROM healthcare.MedicalDepartment) and cityId = ?";
     data = [city];
   }
-  // ANY (SELECT id FROM city)
-  // const query = `SELECT * FROM healthcare.doctor  ${subQuery} Join healthcare.city on healthcare.city.id=healthcare.doctor.cityId Join healthcare.medicaldepartment on healthcare.medicaldepartment.id=healthcare.doctor.departmentId`;
+
   const query = `SELECT healthcare.doctor.id,healthcare.doctor.fullName,healthcare.doctor.email,healthcare.doctor.password,healthcare.doctor.profileImage,healthcare.doctor.gender,healthcare.doctor.status,healthcare.doctor.Nationality,healthcare.doctor.specialization,healthcare.doctor.phone,healthcare.doctor.workingDays,healthcare.doctor.address,healthcare.doctor.careersLicense,healthcare.doctor.waitingTime,healthcare.doctor.consultationFee,healthcare.doctor.ScientificCertificate,healthcare.city.Name as 'city',healthcare.medicaldepartment.Name as 'Department' FROM healthcare.doctor Join healthcare.city on healthcare.city.id=healthcare.doctor.cityId Join healthcare.medicaldepartment on healthcare.medicaldepartment.id=healthcare.doctor.departmentId  where doctor.is_deleted = 0 and (${subQuery})`;
   connection.query(query, data, (err, result) => {
     if (err) {
@@ -169,7 +169,7 @@ const getDoctorByDepartment = (req, res) => {
       return res.status(200).json({ result: [] });
     }
     // result are the data returned by mysql server
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       massage: "All the Doctors in MedicalDepartment = ",
       result: result,
@@ -259,7 +259,7 @@ const deleteDoctorById = (req, res) => {
         err: err,
       });
     }
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       massage: `Succeeded to delete Doctor with id: ${id}`,
       results: results,
@@ -286,6 +286,7 @@ const getDoctorByName = (req, res) => {
     });
   });
 };
+
 //===================================================//Get DoctorProfileImageByID
 
 const DoctorProfileImageByID = (req, res) => {
@@ -308,6 +309,8 @@ const DoctorProfileImageByID = (req, res) => {
   });
 };
 
+//====================================================// module.exports
+
 module.exports = {
   createNewDoctor,
   getDoctorById,
@@ -317,5 +320,5 @@ module.exports = {
   getDoctorByName,
   getDoctorByDepartment,
   DoctorProfileImageByID,
-  getAllDoctorsInAdmin
+  getAllDoctorsInAdmin,
 };
