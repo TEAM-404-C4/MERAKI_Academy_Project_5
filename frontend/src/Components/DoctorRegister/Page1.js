af89b407cc36b81a6616708ed83868d9bfdb63d1
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-02-18 00:07:18 +0200

    DOCTOR register page 1 FRONTEND

--- frontend/src/Components/DoctorRegister/Page1.js
+++ frontend/src/Components/DoctorRegister/Page1.js
@@ -4,49 +4,28 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addInfoPage } from "../Reducer/DoctorRegister/index";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
-import axios from "axios";
 import Swal from "sweetalert2";
-
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-//CSS File
 import "./Page1.css";
 
-//====================================================//Page 1 Function
+//====================================================//Page 1 COMPONENT
 const Page1 = () => {
+  //====================================================//useSelector
+
   const state = useSelector((state) => {
     return state.doctorRegReducer.doctorInfo;
   });
 
-  // =======================================================
+  //====================================================//USESTATE
+
   const [fullName, setFullName] = useState(state.fullName);
   const [email, setEmail] = useState(state.email);
   const [password, setPassword] = useState("");
-  const [image, setImage] = useState("");
   const [profileImage, setprofileImage] = useState(
     "https://i.ibb.co/5YV7j9Z/Male-doctor-with-stethoscope-avatar-Health-care-services-concept-Vector-illustration.jpg"
   );
-  const [imageSelected, setImageSelected] = useState();
-  const [message, setMessage] = useState("");
-  const uploudImage = () => {
-    const formData = new FormData();
-    formData.append("file", imageSelected);
-    formData.append("upload_preset", "yoficjpc");
-    axios
-      .post("https://api.cloudinary.com/v1_1/dbnsxsigi/image/upload", formData)
-      .then((response) => {
-        setprofileImage(response.data.url);
-        console.log(response.data.url, profileImage);
-        setTimeout(() => {
-          dispatch(addInfoPage({ fullName, email, password, profileImage }));
-          console.log(profileImage);
-          history("/doctorsignup2");
-        }, 7000);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+
   //====================================================//Dispatch & Navigate
+
   const dispatch = useDispatch();
   const history = useNavigate();
 
@@ -86,27 +65,9 @@ const Page1 = () => {
         timer: 2000,
       });
     }
-    // const metadata = {
-    //   contentType: 'image/jpeg',
-    // };
-    // const imageRef = ref(storage, `imagesDoctor/${fullName}`);
-    // uploadBytes(imageRef, image,metadata).then(() => {
-    //   getDownloadURL(imageRef).then((url) => {
-    //     setURL(url);
-    //   }).catch((error) => {
-    //     // I think make alert for Error
-    //     console.log(error.message);
-    //   });
-    // }).catch((error) => {
-    //   // I think make alert for Error
-    //   console.log(error.message);
-    // })
-    // uploudImage();
   };
 
-  //====================================================//Return
-
-  // =================================================//Uploud image to firebase
+  // =================================================//Uploud image to CLOUDINARY FUNCTION
   const ImageChange = async (e) => {
     try {
       if (e.target.files[0]) {
@@ -125,14 +86,13 @@ const Page1 = () => {
 
         const file = await res.json();
         setprofileImage(file.secure_url);
-        console.log(file.secure_url, profileImage);
       }
     } catch (err) {
       console.log(err);
     }
   };
 
-  // =================================================//Uploud image to firebase
+  //====================================================//RETURN
   return (
     <>
       <div className="mainPage1">
